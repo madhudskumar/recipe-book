6cc91687ace6cbfdb77e1fc120136e4d0ee8523f
Madhu Kumar D S <madhudskumar@live.in>
2017-01-15 00:27:34 +0530

    restructute and optimised

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {Ingredient} from "../shared/ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list',
   templateUrl: './shopping-list.component.html'
 })
 export class ShoppingListComponent implements OnInit {
-  items: Ingredient[] = [
-    new Ingredient('a', 1),
-    new Ingredient('b', 1),
-    new Ingredient('c', 1),
-  ];
-  constructor() { }
+  items: Ingredient[] = [];
+  constructor(private sls:ShoppingListService) { }
 
   ngOnInit() {
+    this.items = this.sls.getItmes();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
+import {Ingredient} from "../shared/ingredient";
 export class ShoppingListService {
+  private items:Ingredient[] = [];
 
   constructor() { }
 
+  getItmes() {
+    return this.items;
+  }
+
+  addItmes(item: Ingredient[]){
+    Array.prototype.push.apply(this.items, item);
+  }
 }
